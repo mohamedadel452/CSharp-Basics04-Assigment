86bf83d593b6bdab65773283c5bbce1b54e0a9cb
محمد عادل محمد بيومي <85137010+mohamedadel452@users.noreply.github.com>
2026-09-16 14:07:51 +0300

    Q1 is answered

--- C#-Basics04-Assigment/Program.cs
+++ C#-Basics04-Assigment/Program.cs
@@ -2,12 +2,22 @@
 
 namespace Session4Assignment
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-           // dev branch created 
+            
+            #region Q1: One-dimensional array
+            Console.WriteLine("Q1: One-dimensional array ");
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine($"Second price (index 1): {prices[1]}\n");
+            #endregion
+        
+        
+        
+        
         }
+
     }
    
 }
