847a7d1487e146bad10c414a59be5e47d9da0567
محمد عادل محمد بيومي <85137010+mohamedadel452@users.noreply.github.com>
2026-09-16 14:18:17 +0300

    Q2 is answered

--- C#-Basics04-Assigment/Program.cs
+++ C#-Basics04-Assigment/Program.cs
@@ -12,10 +12,27 @@ namespace Session4Assignment
             double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine($"Second price (index 1): {prices[1]}\n");
             #endregion
-        
-        
-        
-        
+
+            #region Q2: Multidimensional array
+            Console.WriteLine("Q2: Multidimensional array ");
+            // Create a 2D array to represent the number of copies of books on two shelves
+            // we can delete this new int []
+            int[,] shelfCopies = new int[2, 2] {
+                { 3, 5 }, // Shelf 0    
+                { 1, 4 }  // Shelf 1
+            };
+
+            Console.WriteLine($"Copies on shelf 0, slot 0: {shelfCopies[0, 0]}");
+            Console.WriteLine($"Copies on shelf 0, slot 1: {shelfCopies[0, 1]}");
+            Console.WriteLine($"Copies on shelf 1, slot 0: {shelfCopies[1, 0]}");
+            Console.WriteLine($"Copies on shelf 1, slot 1: {shelfCopies[1, 1]}");
+           
+            #endregion
+
+
+
+
+
         }
 
     }
